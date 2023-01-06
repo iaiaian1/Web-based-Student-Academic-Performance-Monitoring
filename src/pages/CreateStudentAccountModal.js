f2ff4b84b5a65a0ddbbe5078a16ec3e66d155077
iaiaian1 <yapjakebrian1@gmail.com>
2023-01-06 19:44:49 +0800

    List style view for student focus activities

--- src/pages/CreateStudentAccountModal.js
+++ src/pages/CreateStudentAccountModal.js
@@ -71,10 +71,10 @@ const CreateStudentAccountModal = ({ open, children, onClose, getAccounts }) => 
                         <input
                             type="submit"
                             value="Create account"
-                            className="self-center rounded-lg bg-green-300 py-2 px-5 font-bold"
+                            className="self-center rounded-lg bg-green-500 py-2 px-5 font-bold"
                             onClick={() => createAccount()}
                         />
-                        <button onClick={() => {onClose();}} className=" rounded-lg bg-green-300 py-2 px-5 font-bold">CLOSE</button>
+                        <button onClick={() => {onClose();}} className=" rounded-lg bg-red-500 py-2 px-5 font-bold">CLOSE</button>
                     </div>
                 </form>
             </div>
